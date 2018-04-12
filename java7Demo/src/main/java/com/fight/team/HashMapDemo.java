55313063314737330417e106ce4c51f587440b2a
bailiang <422177371@qq.com>
2018-04-12 09:47:00 +0800

    bail: 1.增加java7模块,准备用于对比1.7与1.8的一些区别示例

---
+++ java7Demo/src/main/java/com/fight/team/HashMapDemo.java
@@ -0,0 +1,23 @@
+package com.fight.team;
+
+import java.util.HashMap;
+
+/**
+ * @author bail
+ * @time 2018/3/28.18:02
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+        //调试Map
+        HashMap<String,String> settings = new HashMap<>(4);
+        settings.put("name","柏亮");
+        settings.put("sex","男");
+        System.out.println(settings.get("name"));
+
+        settings.put("id","1001");
+        settings.put("height","175cm");
+        settings.put("weight","70kg");
+
+
+    }
+}
