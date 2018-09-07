92997c103ef6f0e488da4bc3907a5fe6e3074e24
柏亮 <422177371@qq.com>
2018-09-07 17:21:37 +0800

    修改成方法

--- src/main/java/com.fight.team/bloom/BloomDemo.java
+++ src/main/java/com.fight.team/bloom/BloomDemo.java
@@ -1,46 +1,55 @@
 package com.fight.team.bloom;
 
+import com.google.common.base.Charsets;
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- * 布隆过滤器demo
+ *
  *
  * @author bail
  * @date 2018/6/6
  */
 public class BloomDemo {
-	private static int size = 1000000;
-
-	private static BloomFilter<Integer> bloomFilter = BloomFilter
-			.create(Funnels.integerFunnel(), size);
+	private static int size = 10000000;
+	private static BloomFilter<String> bloomFilter = BloomFilter
+			.create(Funnels.stringFunnel(Charsets.UTF_8), size);
 
 	public static void main(String[] args) {
-		// 创建布隆过滤器,塞入100w个值
+		bloomFunc();
+	}
+
+	private static void bloomFunc(){
+		// 1.创建布隆过滤器,塞入100w个值
+		String strTmp;
 		for (int i = 0; i < size; i++) {
-			bloomFilter.put(i);
+			strTmp = UUID.randomUUID().toString();
+			bloomFilter.put(strTmp);
 		}
+
+		// 2.判断这一百万个数中是否包含29999这个数
 		long startTime = System.nanoTime();
-		// 判断这一百万个数中是否包含29999这个数
-		if (bloomFilter.mightContain(29999)) {
+		if (bloomFilter.mightContain("29999")) {
 			System.out.println("bingo!");
 		}
 		long endTime = System.nanoTime();
-		// 计算时间
-		System.out.println("过滤需要的时间为： " + (endTime - startTime)/1000000 + "毫秒");
+		// 3.计算过滤需要的时间
 
-		List<Integer> list = new ArrayList<>(10000);
+		System.out.println("bloom过滤需要的时间为： " + (endTime - startTime) + "纳秒");
+
+		// 4.计算误判率
+		int errorCount = 0;
 		// 故意取1w个不在过滤器里的值，看看有多少个会被认为在过滤器里
-		for (int i = size + 10000; i < size + 20000; i++) {
-			if (bloomFilter.mightContain(i)) {
-				list.add(i);
+		for (int i = 0; i < 10000; i++) {
+			if (bloomFilter.mightContain(i+"")) {
+				errorCount++;
 			}
 		}
 		// 默认误判率是0.03,误判率越低 -> bitmap长度越大 , 反之也成立
-		System.out.println("误判率：" + list.size() / 10000f);
+		System.out.println("误判率：" + errorCount / 10000f);
 
+		System.out.println(bloomFilter.approximateElementCount());
 	}
 }
